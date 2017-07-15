b5c5c975ab9e13149e1ea767903e5fad49efa466
Kent Carumba <ragnarokfenrir5@yahoo.com.ph>
2017-07-15 15:26:52 +0800

    Test Population for list tab

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Icon } from 'react-native-elements';
+import { StyleSheet, View,ListView,Text } from 'react-native';
+import { Icon,List,ListItem } from 'react-native-elements';
 
 class ListScreen extends React.Component {
   static navigationOptions = {
@@ -13,11 +13,46 @@ class ListScreen extends React.Component {
       />
     )
   };
+ constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
 
-  render() {
+    this.state = {
+              list : [{
+                  name: 'Amy Farha',
+                  avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                  subtitle: 'Vice President'
+                },    
+                {
+                  name: 'Chris Jackson',
+                  avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+                  subtitle: 'Vice Chairman'
+                }]
+         }
+
+    
+  }
+  componentDidMount(){
+
+  }
+  render(){
+    var listItems = this.state.list.map((l,i)=>{
+      return (<ListItem
+        roundAvatar
+        avatar={{uri:l.avatar_url}}
+        key={i}
+        title={l.name}
+        subtitle={l.subtitle} />
+      )
+    });
     return (
-      <View></View>
-    );
+        <List containerStyle={{marginBottom: 20}} >
+          {listItems}
+        </List>
+      );
   }
 }
 
