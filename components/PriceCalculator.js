e89720eb6cdfa66c025bdd1deba6461925f58d92
Kent Carumba <ragnarokfenrir5@yahoo.com.ph>
2017-07-16 03:26:16 +0800

    Changed years to paycomponent and Downpayment component to sliders

--- components/PriceCalculator.js
+++ components/PriceCalculator.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet,Image } from 'react-native';
-import {FormLabel,FormInput,Button} from 'react-native-elements'
+import { View, Text, StyleSheet,Image } from 'react-native';
+import {FormLabel,FormInput,Button,Slider} from 'react-native-elements'
+
+
+const MINIMUM_YEARS_TO_PAY = 1
+const MAXIMUM_YEARS_TO_PAY = 20
+const MINIMUM_DOWNPAYMENT = 10
+const MAXIMUM_DOWNPAYMENT = 80
+
+
+
 
 const PriceCalculator = props => {
   return (
   	<View>
         <FormLabel>Price</FormLabel>
-        <FormInput editable={true} maxLength={40} value={props.price} onChangeText={props.setPrice} keyboardType={"numeric"} />
-        <FormLabel>Months to Pay</FormLabel>
-        <FormInput editable={true} maxLength={40} value={props.months_to_pay} onChangeText={props.setMonthstoPay} keyboardType={"numeric"} maxLength={2} />
-        <FormLabel>Downpayment in %</FormLabel>
-        <FormInput editable={true} maxLength={40} value={props.downpayment} onChangeText={props.setDownpayment} maxLength={2} keyboardType={"numeric"} />
-        <Button style={{marginTop:10}}
-                title="Calculate" backgroundColor={"#90CAF9"} onPress={props.calculateMonthlyPrice} />
+        <FormInput 
+            editable={true} 
+            maxLength={40} 
+            value={props.price} 
+            onChangeText={props.setPrice} 
+            keyboardType={"numeric"} />
+        <FormLabel>Years to Pay: {props.yearsToPay}
+        </FormLabel>
+        
+        <Slider value={props.yearsToPay}
+                step={1}
+                minimumValue={MINIMUM_YEARS_TO_PAY}
+                maximumValue={MAXIMUM_YEARS_TO_PAY}
+                onValueChange={props.setYearsToPay}/>
+        <FormLabel>Downpayment: {props.downpayment}%</FormLabel>
+        
+        <Slider value={props.downpayment}
+                minimumValue={MINIMUM_DOWNPAYMENT}
+                maximumValue={MAXIMUM_DOWNPAYMENT}
+                step={1}
+                onValueChange={props.setDownPayment}/>
+        
+        <Button style={{marginTop:10, marginBottom: 10}}
+                title="Calculate" 
+                backgroundColor={"#90CAF9"} 
+                onPress={props.calculateMonthlyPrice} />
+
+        <Text>Calculated Price: {props.calculatedPrice}</Text>
     </View>
   );
 }
