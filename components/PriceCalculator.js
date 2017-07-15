7a7a80b0e0cd1d62057ae0185db592821b916a55
Kent Carumba <ragnarokfenrir5@yahoo.com.ph>
2017-07-15 23:14:43 +0800

    Fixed bug Price,Months to pay, Downpayment accepting letters instead of numbers

--- components/PriceCalculator.js
+++ components/PriceCalculator.js
@@ -1,4 +1,4 @@
-import React,{Dimensions} from 'react';
+import React from 'react';
 import { View, Text, TextInput, StyleSheet,Image } from 'react-native';
 import {FormLabel,FormInput,Button} from 'react-native-elements'
 
@@ -6,12 +6,13 @@ const PriceCalculator = props => {
   return (
   	<View>
         <FormLabel>Price</FormLabel>
-        <FormInput editable={true} maxLength={40} value={props.price} onChangeText={props.setPrice}/>
+        <FormInput editable={true} maxLength={40} value={props.price} onChangeText={props.setPrice} keyboardType={"numeric"} />
         <FormLabel>Months to Pay</FormLabel>
-        <FormInput editable={true} maxLength={40} value={props.months_to_pay} onChangeText={props.setMonthstoPay} />
-        <FormLabel>Downpayment</FormLabel>
-        <FormInput editable={true} maxLength={40} value={props.downpayment} onChangeText={props.setDownpayment} />
-        <Button title="Calculate" backgroundColor={"#90CAF9"} onPress={props.calculateMonthlyPrice} />
+        <FormInput editable={true} maxLength={40} value={props.months_to_pay} onChangeText={props.setMonthstoPay} keyboardType={"numeric"} maxLength={2} />
+        <FormLabel>Downpayment in %</FormLabel>
+        <FormInput editable={true} maxLength={40} value={props.downpayment} onChangeText={props.setDownpayment} maxLength={2} keyboardType={"numeric"} />
+        <Button style={{marginTop:10}}
+                title="Calculate" backgroundColor={"#90CAF9"} onPress={props.calculateMonthlyPrice} />
     </View>
   );
 }
