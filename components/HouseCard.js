8220c37e35e030b69ff6448012c69aa5d9ec424e
Kent Carumba <ragnarokfenrir5@yahoo.com.ph>
2017-07-15 21:27:00 +0800

    Added card view for list

--- components/HouseCard.js
+++ components/HouseCard.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React,{Dimensions} from 'react';
+import { View, Text, TextInput, StyleSheet,Image } from 'react-native';
+import {Card} from 'react-native-elements'
 
 const HouseCard = props => {
   return (
-    <Text>{props.price}</Text>
+  	<Card>
+  	<Image style={{flex:1}}
+  			resizeMode={"cover"}
+  			source={{uri: props.image}} />
+    <View style={{marginTop:20}}></View>
+    <Text>Prices: {props.price}</Text>
+  	<Text>Description: {props.description}</Text>
+  	<Text>type: {props.type}</Text>
+  	
+    </Card>
   );
 }
+
+
 
 export default HouseCard;
--- components/ListComponent.js
+++ components/ListComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet,ScrollView } from 'react-native';
 import HouseCard from './HouseCard';
 
 const ListComponent = props => {
   return (
-    <View>
+
+    <ScrollView>
       {props.houses.length > 0 ? props.houses.map(house => <HouseCard key={house.id} {...house}></HouseCard>) : null}
-    </View>
+    </ScrollView>
   )
 }
 
