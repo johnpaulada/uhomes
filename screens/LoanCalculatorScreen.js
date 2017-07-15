008d8395cdb801413d0ae9065b18c03a70de3df9
John Paul Ada <johnpaulada@gmail.com>
2017-07-15 09:09:57 -0400

    Added LoanCalculatorScreen

---
+++ screens/LoanCalculatorScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+class LoanCalculatorScreen extends React.Component {
+  render() {
+    return (
+      <View></View>
+    );
+  }
+}
+
+export default LoanCalculatorScreen;
